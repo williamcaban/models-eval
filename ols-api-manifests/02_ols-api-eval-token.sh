0e62d37d262aad2f34016b01a0e15050560257a9
William Caban <william.caban@gmail.com>
2024-07-20 13:16:35 -0400

    Updating evaluation scripts and adding Jupyter notebook

--- ols-api-manifests/02_ols-api-eval-token.sh
+++ ols-api-manifests/02_ols-api-eval-token.sh
@@ -7,6 +7,22 @@ SA_QUERY="sa-ols-query"
 oc create sa $SA_QUERY
 oc create clusterrolebinding $SA_QUERY --clusterrole=$OLS_ROLE --serviceaccount=openshift-lightspeed:$SA_QUERY
 
+# Option 1
 export TOKEN=$(oc create token -n openshift-lightspeed $SA_QUERY --duration=0s)
 
-echo "OLS_API_TOKEN="$TOKEN > OLS_API_TOKEN.txt
+# Option 2
+oc apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $SA_QUERY-secret
+  namespace: openshift-lightspeed
+  annotations:
+    kubernetes.io/service-account.name: $SA_QUERY
+type: kubernetes.io/service-account-token
+EOF
+
+export TOKEN2=$(oc get secret $SA_QUERY-secret -o jsonpath='{.data.token}')
+
+echo "OLS_API_TOKEN  = "$TOKEN  >  OLS_API_TOKEN.txt
+echo "OLS_API_TOKEN2 = "$TOKEN2 >> OLS_API_TOKEN.txt
